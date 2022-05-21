e364a1a3b8a136be0f6365ddd3950e398561de26
Mark Wilgelm <markujke@mail.ru>
2022-05-21 10:25:13 +0200

    Initial Commit

---
+++ Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Mark Wilhelm on 21.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
