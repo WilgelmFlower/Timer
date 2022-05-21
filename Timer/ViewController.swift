c4ef39e4380e84a16c2112068bd6ecad181073b6
Mark Wilgelm <markujke@mail.ru>
2022-05-21 23:39:39 +0200

    work time timer is done

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -1,19 +1,40 @@
-//
-//  ViewController.swift
-//  Timer
-//
-//  Created by Mark Wilhelm on 21.05.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var timer = Timer()
+    var isStarted: Bool = false
+    var seconds = 1500
+        {
+        didSet {
+            timeLabel.text = ("\(seconds / 60):\(seconds % 60)")
+        }
+        }
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startStopButton: UIButton!
+    @IBAction func startStopButton(_ sender: Any)
+        {
+        if (isStarted)
+        {
+            isStarted = false
+            timer.invalidate()
+            startStopButton.setTitle("Start", for: .normal)
+            
+        } else {
+            isStarted = true
+            startStopButton.setTitle("Stop", for: .normal)
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(tick), userInfo: nil, repeats: true )
+        }
+        
+        }
+    
+    @objc func tick()
+        {
+        seconds -= 1
+        if seconds == 0 {
+            timer.invalidate()
+        }
     }
-
-
 }
 
