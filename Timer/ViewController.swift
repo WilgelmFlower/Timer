275fc14af714f3f2ac206795abe2e8d2e9bf7ebb
Mark Wilgelm <markujke@mail.ru>
2022-05-26 12:06:02 +0200

    main work is done, timer ready

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -4,17 +4,15 @@ class ViewController: UIViewController {
     
     var timer = Timer()
     var isStarted: Bool = false
-    var seconds = 1500
-        {
-        didSet {
-            timeLabel.text = ("\(seconds / 60):\(seconds % 60)")
-        }
-        }
+    var isWorkTime: Bool = true
+    var secondsCurrent = 0
+    let secondsRest = 300
+    let secondsWork = 1500
     
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var startStopButton: UIButton!
     @IBAction func startStopButton(_ sender: Any)
-        {
+    {
         if (isStarted)
         {
             isStarted = false
@@ -26,15 +24,25 @@ class ViewController: UIViewController {
             startStopButton.setTitle("Stop", for: .normal)
             timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(tick), userInfo: nil, repeats: true )
         }
-        
-        }
+    }
     
     @objc func tick()
-        {
-        seconds -= 1
-        if seconds == 0 {
-            timer.invalidate()
+    {
+        secondsCurrent += 100
+        if (isWorkTime) {
+            let workTime = secondsWork - secondsCurrent
+            timeLabel.text = (String(format:"%02i:%02i", workTime / 60, workTime % 60))
+            if workTime == 0{
+                isWorkTime = false
+                secondsCurrent = 0
+            }
+        } else {
+            let restTime = secondsRest - secondsCurrent
+            timeLabel.text = (String(format:"%02i:%02i", restTime / 60, restTime % 60))
+            if restTime == 0 {
+                isWorkTime = true
+                secondsCurrent = 0
+            }
         }
     }
 }
-
